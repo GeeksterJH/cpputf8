a9ae4f8e7c8ee45de6036b94fb4f11ec35b96f90
Joren Heyman <joren@heyman.io>
2020-12-19 14:49:30 +0100

    fix conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class CppUtf8Conan(ConanFile):
     name = "cpputf8"
     version = "0.1.0"
 
-    exports_sources = "include/*", "CMakeLists.txt", "cmake/*", "test/*"
+    exports_sources = "include/*", "CMakeLists.txt", "cmake/*", "test/*", "build/include/*"
     no_copy_sources = True
 
     def build(self):
@@ -13,7 +13,8 @@ class CppUtf8Conan(ConanFile):
         cmake.configure()
 
     def package(self):
-        self.copy("*.hpp")
+        self.copy("*.hpp", dst="include", src="include")
+        self.copy("*.hpp", dst="include", src="build/include")
 
     def package_id(self):
         self.info.header_only()
