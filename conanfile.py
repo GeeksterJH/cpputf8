aac021a73633435e047dad565f06096afbb85adf
Joren Heyman <joren@heyman.io>
2020-12-19 17:18:53 +0100

    build cmake project in conanfile

--- conanfile.py
+++ conanfile.py
@@ -5,16 +5,25 @@ class CppUtf8Conan(ConanFile):
     name = "cpputf8"
     version = "0.1.0"
 
-    exports_sources = "include/*", "CMakeLists.txt", "cmake/*", "test/*", "build/include/*"
+    exports_sources = [
+        "include/*",
+        "CMakeLists.txt",
+        "cmake/*",
+        "test/*",
+        "build/include/*",
+        "scripts/*"
+    ]
+
     no_copy_sources = True
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
+        cmake.build()
 
     def package(self):
-        self.copy("*.hpp", dst="include", src="include")
-        self.copy("*.hpp", dst="include", src="build/include")
+        self.copy("*.hpp", src="include", dst="include")
+        self.copy("*.hpp", src="build/include", dst="include")
 
     def package_id(self):
         self.info.header_only()
